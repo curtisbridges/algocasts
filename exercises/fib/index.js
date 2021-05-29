9320b18b667841e16e45e8f42a04b6a34a914af9
Curtis Bridges <curtis@curtisbridges.com>
2021-05-29 18:10:14 -0400

    exercise 13 - fibonacci
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -1,13 +1,27 @@
 // --- Directions
 // Print out the n-th entry in the fibonacci series.
 // The fibonacci series is an ordering of numbers where
-// each number is the sum of the preceeding two.
+// each number is the sum of the preceding two.
 // For example, the sequence
 //  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  // solution 1, iterative
+  // const result = [0, 1]
+
+  // for (let num = 2; num <= n; num++) {
+  //   result.push(result[num - 2] + result[num - 1])
+  // }
+
+  // return result[n]
+
+  // solution 2, recursive
+  if (n < 2) return n
+
+  return fib(n - 1) + fib(n - 2)
+}
 
 module.exports = fib;
