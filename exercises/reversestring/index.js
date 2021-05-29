01425fe2049e1878ef6b1fd49b2d47bfe3d915c2
Curtis Bridges <curtis@curtisbridges.com>
2021-05-29 10:22:30 -0400

    exercise 1 - reverse string
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,19 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // solution 1
+  // return str.split('').reverse().join('')
+
+  // solution 2
+  // let reversed = ''
+  // for (let character of str) {
+  //   reversed = character + reversed
+  // }
+  // return reversed
+
+  // solution 3
+  return str.split('').reduce((reversed, character) => character + reversed, '')
+}
 
 module.exports = reverse;
