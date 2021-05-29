87c40b510758828213ffb4144cc6c05db7ee1ce9
Curtis Bridges <curtis@curtisbridges.com>
2021-05-29 13:25:23 -0400

    exercise 10 - pyramids
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,20 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // total = 1:1, 2:3, 3:5
+  // spaces left, right = 1:0, 2:1, 3:2...
+  // hashes = 1:1, 2:3, 3:5...
+  const numChars = 2 * n - 1
+  for (let i = 1; i <= n; i++) {
+    const numHashes = 2 * i - 1
+    const numSpaces = Math.floor((numChars - numHashes) / 2)
+
+    const spaces = ' '.repeat(numSpaces)
+    const hashes = '#'.repeat(numHashes)
+
+    console.log(spaces + hashes + spaces)
+  }
+}
 
 module.exports = pyramid;
