4b1391cc2ca342a6e5a067e6b7b1d2346a9a5166
Curtis Bridges <curtis@curtisbridges.com>
2021-05-29 13:36:03 -0400

    exercise 11 - vowels
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const vowels = 'aeiou'
+
+  let numVowels = 0
+  for (const vowel of vowels) {
+    numVowels += str.toLowerCase().split(vowel).length - 1
+  }
+
+  return numVowels
+
+  // solution 2, regex!
+  // const matches = str.match(/[aeiou]/gi)
+  // return matches ? matches.length : 0
+}
 
 module.exports = vowels;
