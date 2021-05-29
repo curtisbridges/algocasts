96b065bdb9852efdad4544742f8fb5f5cd46c080
Curtis Bridges <curtis@curtisbridges.com>
2021-05-29 11:52:02 -0400

    exercise 7 - anagrams
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,12 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return normalize(stringA) === normalize(stringB)
+}
+
+function normalize(string) {
+  return string.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
 
 module.exports = anagrams;
