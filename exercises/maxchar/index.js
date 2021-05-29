2b8d284e35226e70b8580e7fa9b44163bea1d042
Curtis Bridges <curtis@curtisbridges.com>
2021-05-29 11:14:53 -0400

    exercise 4 - max char occurences in string
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const map = {}
+  for (const character of str) {
+    if (map[character]) {
+      map[character]++
+    } else {
+      map[character] = 1
+    }
+  }
+
+  let maxChar = str[0]
+  for (const key in map) {
+    if (map[key] > map[maxChar]) {
+      maxChar = key
+    }
+  }
+  return maxChar
+}
 
 module.exports = maxChar;
