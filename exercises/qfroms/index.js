991a3891f19e96e6721c6986874ad93c98abb7dd
Curtis Bridges <curtis@curtisbridges.com>
2021-05-29 19:14:12 -0400

    exercise 17 - queue from stacks
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Implement a Queue datastructure using two stacks.
+// Implement a Queue data structure using two stacks.
 // *Do not* create an array inside of the 'Queue' class.
 // Queue should implement the methods 'add', 'remove', and 'peek'.
 // For a reminder on what each method does, look back
@@ -14,6 +14,39 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.s1 = new Stack()
+    this.s2 = new Stack()
+  }
+
+  add(record) {
+    this.s1.push(record)
+  }
+
+  remove() {
+    while (this.s1.peek()) {
+      this.s2.push(this.s1.pop())
+    }
+    const needle = this.s2.pop()
+    while (this.s2.peek()) {
+      this.s1.push(this.s2.pop())
+    }
+
+    return needle
+  }
+
+  peek() {
+    while (this.s1.peek()) {
+      this.s2.push(this.s1.pop())
+    }
+    const needle = this.s2.peek()
+    while (this.s2.peek()) {
+      this.s1.push(this.s2.pop())
+    }
+
+    return needle
+  }
+}
 
 module.exports = Queue;
