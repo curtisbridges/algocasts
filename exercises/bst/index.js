7bbe3168a9db07f659fc3483123223510795e9ad
Curtis Bridges <curtis@curtisbridges.com>
2021-05-31 12:45:33 -0400

    exercise 24 - binary search trees
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,42 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+
+  insert(data) {
+    if (this.data > data) {
+      // add to the left
+      if (this.left) {
+        this.left.insert(data)
+      } else {
+        this.left = new Node(data)
+      }
+    } else {
+      // add to the right
+      if (this.right) {
+        this.right.insert(data)
+      } else {
+        this.right = new Node(data)
+      }
+    }
+  }
+
+  contains(data) {
+    if (data === this.data) {
+      return this
+    } else if (this.left && data < this.data) {
+      return this.left.contains(data)
+    } else if (this.right && data > this.data) {
+      return this.right.contains(data)
+    } else {
+      return null
+    }
+  }
+}
 
 module.exports = Node;
